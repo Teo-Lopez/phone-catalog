d6c6c9cab90fc5dc0f1a6b21a343cd460f0df6cb
Teo <teodorolg130@gmail.com>
2020-02-05 12:29:16 +0100

    Deploying

---
+++ server/public/precache-manifest.c048adb4e2f6962d18198ff5f5e104fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61ed7fbab6740d5487f33a261c6d2a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce9e20e58762d15e873",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a46605e80bab1ccbbc6b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5ce9e20e58762d15e873",
+    "url": "/static/js/2.06f02a2e.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.06f02a2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a46605e80bab1ccbbc6b",
+    "url": "/static/js/main.4765b3d9.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
